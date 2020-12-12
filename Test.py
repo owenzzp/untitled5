3692bd3704df2f305a1e44258670279e6dd6af50
MI <2812425823@qq.com>
2020-12-12 10:34:00 +0800

    更新Test.py

--- Test.py
+++ Test.py
@@ -5,13 +5,26 @@ Created on 2016-7-26
 Project:编写Web测试用例
 '''
 import sys,os
-sys.path.append("/root/dingdian/dingdian/")
+sys.path.append('C:\\Users\\MI\\PycharmProjects\\untitled5')
+
+sys.path.append('C:\\Users\\MI\\PycharmProjects\\untitled5')
+sys.path.append('C:\\Program Files\\JetBrains\\PyCharm 2019.2.5\\helpers\\pycharm_display')
+sys.path.append('C:\\Users\\MI\\AppData\\Local\\Programs\\Python\\Python37\\python37.zip')
+sys.path.append('C:\\Users\\MI\\AppData\\Local\\Programs\\Python\\Python37\\DLLs')
+sys.path.append('C:\\Users\\MI\\AppData\\Local\\Programs\\Python\\Python37\\lib')
+sys.path.append('C:\\Users\\MI\\AppData\\Local\\Programs\\Python\\Python37')
+sys.path.append('C:\\Users\\MI\\AppData\\Roaming\\Python\\Python37\\site-packages')
+sys.path.append('C:\\Users\\MI\\AppData\\Local\\Programs\\Python\\Python37\\lib\\site-packages')
+sys.path.append('C:\\Program Files\\JetBrains\\PyCharm 2019.2.5\\helpers\\pycharm_matplotlib_backend')
+
 import unittest
 from HTMLTestRunner import HTMLTestRunner
 import Testbaidu
 import Testyoudao
 
-#构造测试集
+print(sys.path)
+
+# #构造测试集
 suite = unittest.TestSuite()
 # 添加测试案例
 suite.addTest(Testbaidu.BaiduTest('test_baidu'))
